494c8d64ed0eb8d232abf919efa333a16c3b6620
Edvaldo Junior <EJUNIOR@volvocars.com>
2022-04-21 22:34:49 +0200

    WIP

---
+++ service/src/Finance.Tests/Infrastructure/DatabaseFactory.cs
@@ -0,0 +1,49 @@
+﻿namespace Finance.Tests.Infrastructure
+{
+    using Finance.Infrastructure.Data.UnitOfWork;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Data.SqlClient;
+
+    public class DatabaseFactory
+    {
+        private readonly SqlConnection _connection;
+
+        public DatabaseFactory()
+        {
+            _connection =
+                new SqlConnection(ConnectionString);
+
+            CreateDatabase();
+        }
+
+        private static string ConnectionString
+            => @"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=sql-finance-tests;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public SqlConnection GetConnection()
+        {
+            return _connection;
+        }
+
+        private void CreateDatabase()
+        {
+            var context
+                = GetDbContext();
+
+            context
+                .Database
+                .EnsureDeleted();
+
+            context
+                .Database
+                .Migrate();
+        }
+
+        private FinanceUnitOfWork GetDbContext()
+        {
+            return new FinanceUnitOfWork(
+                new DbContextOptionsBuilder<FinanceUnitOfWork>()
+                    .UseSqlServer(ConnectionString)
+                    .Options);
+        }
+    }
+}
---
+++ service/src/Finance.Tests/Infrastructure/FixtureBase.cs
@@ -0,0 +1,22 @@
+﻿namespace Finance.Tests.Infrastructure
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.TestHost;
+
+    public class FixtureBase
+    {
+        private readonly DatabaseFactory _databaseFactory = new DatabaseFactory();
+
+        private Dictionary<string, string> BuildInMemoryCollection()
+        {
+            return new Dictionary<string, string>
+            {
+                {"DatabaseConnectionString", _databaseFactory.GetConnection().ConnectionString}
+            };
+        }
+
+        private TestServer CreateTestServer()
+        {
+        }
+    }
+}
