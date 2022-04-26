0399e7200d687d7bcc1c5c0164bbeeeac3dad339
Edvaldo Junior <EJUNIOR@volvocars.com>
2022-04-26 21:03:36 +0200

    Infrastrucuture

---
+++ service/src/Finance.Tests/Category/CategoryControllerTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Finance.Tests.Category
+{
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class CategoryControllerTests
+    {
+        //Metodo - Grantir um comportamento
+        // Arrange / Act / Assert
+        [Fact]
+        public async Task ShouldGetCategory()
+        {
+        }
+    }
+}
--- service/src/Finance.Tests/Infrastructure/FixtureBase.cs
+++ service/src/Finance.Tests/Infrastructure/FixtureBase.cs
@@ -1,11 +1,35 @@
 ﻿namespace Finance.Tests.Infrastructure
 {
+    using System;
     using System.Collections.Generic;
+    using System.Net.Http;
+    using Api;
+    using Finance.Api;
+    using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.TestHost;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
 
     public class FixtureBase
     {
+        private readonly TestServer _apiServer;
         private readonly DatabaseFactory _databaseFactory = new DatabaseFactory();
+
+        public FixtureBase()
+        {
+            _apiServer = CreateTestServer();
+        }
+
+        public HttpClient CreateClient()
+        {
+            var client = _apiServer
+                .CreateClient();
+
+            client.Timeout = TimeSpan
+                .FromMinutes(5);
+
+            return client;
+        }
 
         private Dictionary<string, string> BuildInMemoryCollection()
         {
@@ -17,6 +41,28 @@
 
         private TestServer CreateTestServer()
         {
+            var builder = new WebHostBuilder()
+                .UseConfiguration(new ConfigurationBuilder()
+                    .AddInMemoryCollection(BuildInMemoryCollection())
+                    .Build())
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    context
+                        .HostingEnvironment
+                        .EnvironmentName = "Test";
+                })
+                .UseContentRoot(AppContext.BaseDirectory)
+                .ConfigureTestServices(services =>
+                {
+                    //Configuracao de Mock Objects
+
+                    services
+                        .AddControllers()
+                        .AddApplicationPart(typeof(Startup).Assembly);
+                })
+                .UseStartup(typeof(ApiTestStartup));
+
+            return new TestServer(builder);
         }
     }
 }
