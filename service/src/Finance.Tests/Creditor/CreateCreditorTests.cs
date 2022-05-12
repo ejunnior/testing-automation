6b73346d32fb6d09e270bc6f3be084dac7a3b632
Edvaldo Junior <EJUNIOR@volvocars.com>
2022-05-12 22:16:11 +0200

    WIP

---
+++ service/src/Finance.Tests/Creditor/CreateCreditorTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Finance.Tests.Creditor
+{
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using Xunit;
+
+    public class CreateCreditorTests
+    {
+        private const string PageUrl = "http://localhost:4200/";
+        private readonly IWebDriver _webDriver;
+
+        public CreateCreditorTests()
+        {
+            _webDriver = new ChromeDriver();
+        }
+
+        [Fact]
+        private void ShouldCreditorBeCreated()
+        {
+            // Arrange
+            _webDriver.Navigate().GoToUrl(PageUrl);
+
+            _webDriver.FindElement(By.Id("creditor")).Click();
+
+            _webDriver.FindElement(By.Id("create")).Click();
+
+            _webDriver.FindElement(By.Id("name")).SendKeys("Creditor Name");
+
+            _webDriver.FindElement(By.Id("create")).Click();
+
+            // Act
+
+            // Assert
+        }
+    }
+}
